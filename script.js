a04803a31c3dfe0c8eabd03f8b326cf199206e2d
0xKufre <90410673+0xKufre@users.noreply.github.com>
2024-04-01 22:53:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function checkCategory() {
+    // let selectedCategory = document.querySelector('.w--current').innerHTML;
+    // // console.log(selectedCategory)
+    document.getElementById('selected-category').innerText = document.querySelector('.w--current').innerText
+    // console.log(document.querySelector('.w--current').innerText)
+    // console.log('me')
+}
